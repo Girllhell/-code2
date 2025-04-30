68317e80f99162904cc75f606185b4b0d094ddbe
Girllhell <98600894+Girllhell@users.noreply.github.com>
2025-04-30 22:29:45 +0300

    server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname)); // Serve static files
+// استخدم ملفات ثابتة من المجلد الحالي
+app.use(express.static(__dirname));
 
+// الصفحة الرئيسية
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
