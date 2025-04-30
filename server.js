69de2d10380ed5a521030470f5fdcde8a42114c5
Girllhell <98600894+Girllhell@users.noreply.github.com>
2025-04-30 22:41:23 +0300

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-// استخدم ملفات ثابتة من المجلد الحالي
-app.use(express.static(__dirname));
+// نخلي السيرفر يقدم الملفات من نفس المجلد
+app.use(express.static(path.join(__dirname)));
 
-// الصفحة الرئيسية
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
