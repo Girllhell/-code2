17977fa98bf7c22f93c91b3d7e4edf7ede4384f0
Girllhell <98600894+Girllhell@users.noreply.github.com>
2025-04-30 22:44:17 +0300

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = 3000;
 
-// نخلي السيرفر يقدم الملفات من نفس المجلد
-app.use(express.static(path.join(__dirname)));
+// خدمة الملفات الساكنة (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
